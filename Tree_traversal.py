595ccaf4c6e07bb73a7e68ea22db265834ad6251
serizawa limboo <serizawalimboo@gmail.com>
2022-09-18 10:49:49 +0530

    traverse updated

--- Tree_traversal.py
+++ Tree_traversal.py
@@ -10,3 +10,30 @@ def inorder(root):
         #traverse root
         print(str(root.val) + "->", end="")
         inorder(root.right)
+
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(str(root.val) + "->", end="")
+
+def preorder(root):
+    if root:
+        print(str(root.val) + "->", end="")
+        preorder(root.left)
+        preorder(root.right)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Inorder traversal")
+inorder(root)
+
+print("\nPreorder traversal")
+preorder(root)
+
+print("\nPostorder traversal")
+postorder(root)
